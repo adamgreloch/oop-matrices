d6b2cd407a9f9001ca7385f46f5649a360ea5dac
Adam Greloch <zplhatesbananas@gmail.com>
2022-05-10 21:35:04 +0200

    implement FullMatrix

--- src/main/java/pl/edu/mimuw/matrix/FullMatrix.java
+++ src/main/java/pl/edu/mimuw/matrix/FullMatrix.java
@@ -4,16 +4,16 @@ public class FullMatrix implements IDoubleMatrix {
   private final double[][] values;
   private final Shape shape;
 
+  public FullMatrix(double[][] values) {
+    this.shape = Shape.matrix(values.length, values[0].length);
+    this.values = copy2DTable(values);
+  }
+
   private static double[][] copy2DTable(double[][] table) {
     double[][] res = new double[table.length][];
     for (int i = 0; i < table.length; i++)
       res[i] = table[i].clone();
     return res;
-  }
-
-  public FullMatrix(double[][] values) {
-    this.shape = Shape.matrix(values.length, values[0].length);
-    this.values = copy2DTable(values);
   }
 
   public IDoubleMatrix times(IDoubleMatrix other) {
@@ -31,7 +31,7 @@ public class FullMatrix implements IDoubleMatrix {
   }
 
   public IDoubleMatrix plus(IDoubleMatrix other) {
-    return null;
+    return other.plusFull(this);
   }
 
   public IDoubleMatrix plus(double scalar) {
@@ -44,7 +44,7 @@ public class FullMatrix implements IDoubleMatrix {
   }
 
   public IDoubleMatrix minus(IDoubleMatrix other) {
-    return null;
+    return other.lHMinusFull(this);
   }
 
   public IDoubleMatrix minus(double scalar) {
@@ -60,19 +60,35 @@ public class FullMatrix implements IDoubleMatrix {
   }
 
   public double normOne() {
-    return 0;
+    double max = 0;
+
+    for (int i = 0; i < this.shape().columns; i++)
+      max = Math.max(this.getAbsColumn(i), max);
+
+    return max;
   }
 
   public double normInfinity() {
-    return 0;
+    double max = 0;
+
+    for (int i = 0; i < this.shape().rows; i++)
+        max = Math.max(this.getAbsRow(i), max);
+
+    return max;
   }
 
   public double frobeniusNorm() {
-    return 0;
+    double frob = 0;
+
+    for (int i = 0; i < this.shape().rows; i++)
+      for (int j = 0; j < this.shape().columns; j++)
+        frob += Math.pow(this.get(i, j), 2);
+
+    return Math.sqrt(frob);
   }
 
   public Shape shape() {
-    return null;
+    return this.shape;
   }
 
   public IDoubleMatrix plusSparse(SparseMatrix other) {
@@ -80,11 +96,11 @@ public class FullMatrix implements IDoubleMatrix {
   }
 
   public IDoubleMatrix rHMinusSparse(SparseMatrix other) {
-    return other.rHMinusFull(this);
+    return other.lHMinusFull(this);
   }
 
   public IDoubleMatrix rHTimesSparse(SparseMatrix other) {
-    return other.rHTimesFull(this);
+    return other.lHTimesFull(this);
   }
 
   public IDoubleMatrix plusFull(FullMatrix other) {
@@ -120,6 +136,14 @@ public class FullMatrix implements IDoubleMatrix {
     return new FullMatrix(res);
   }
 
+  public IDoubleMatrix lHMinusFull(FullMatrix other) {
+    return other.rHMinusFull(this);
+  }
+
+  public IDoubleMatrix lHTimesFull(FullMatrix other) {
+    return other.rHTimesFull(this);
+  }
+
   public double getColumn(int column) {
     double sum = 0;
     for (int i = 0; i < this.shape().rows; i++)
@@ -131,6 +155,20 @@ public class FullMatrix implements IDoubleMatrix {
     double sum = 0;
     for (int i = 0; i < this.shape().rows; i++)
       sum += this.get(row, i);
+    return sum;
+  }
+
+  public double getAbsColumn(int column) {
+    double sum = 0;
+    for (int i = 0; i < this.shape().rows; i++)
+      sum += Math.abs(this.get(i, column));
+    return sum;
+  }
+
+  public double getAbsRow(int row) {
+    double sum = 0;
+    for (int i = 0; i < this.shape().rows; i++)
+      sum += Math.abs(this.get(row, i));
     return sum;
   }
 }
